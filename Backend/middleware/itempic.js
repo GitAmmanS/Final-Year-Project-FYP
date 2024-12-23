4f84429a51b6db263505a8b6a2bf71520aba2943
Amman Sajjad <ammansajjad42@gmail.com>
2024-12-23 15:39:17 +0500

    commit from Amman 23/12/2024
    mao nyo

--- Backend/middleware/itempic.js
+++ Backend/middleware/itempic.js
@@ -1,31 +1,22 @@
 const multer = require("multer");
 const path =require("path");
-
+const crypto = require('crypto')
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-            cb(null, path.join(__dirname, '../public'));
-        }else {
-            cb(new Error('Unsupported file type'));
-        }
+       cb(null,'./public')
     },
     filename: function(req, file, cb) {
-        const name = Date.now() + '-' + file.originalname.replace(/\s/g, '');
-        cb(null, name);
+        crypto.randomBytes(12,function(err,name){
+            const fn = name.toString("hex")+path.extname(file.originalname);
+            cb(null,fn)
+        })
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    if ((file.fieldname === "picture" && (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'))){
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+
 
 module.exports = multer({
     storage: storage,
-    fileFilter:fileFilter,
     limits: {
         fileSize: 2 * 1024 * 1024 
     }
